96fa220d8091fe2c8608a3a8e516a8a49c2f3f63
Andri Adam <andriadam2792000@gmail.com>
2023-01-17 12:19:37 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    int num = 10;
+
+    Console.WriteLine("Soal 1 :");
+    for (int i = 1; i <= num; i++)
+    {
+      for (int j = 1; j <= num - i; j++)
+      {
+        Console.Write(" ");
+      }
+      for (int k = 1; k <= (2 * i) - 1; k++)
+      {
+        if (k % 2 == 0)
+          Console.Write(" ");
+        else
+          Console.Write(i);
+      }
+      Console.WriteLine();
+    }
+
+    Console.WriteLine("\nSoal 2 :");
+    for (int i = 1; i <= num; i++)
+    {
+      for (int j = i; j <= num; j++)
+      {
+        Console.Write(j);
+      }
+      Console.WriteLine();
+    }
+  }
+
+  
+}
